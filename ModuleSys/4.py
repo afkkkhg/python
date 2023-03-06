5ecbe3205e3fa940af3881b649598d1abd7ef0ca
Kuchmasova Sofia <114149918+afkkkhg@users.noreply.github.com>
2023-03-06 14:09:50 +0300

    Update 4.py

--- ModuleSys/4.py
+++ ModuleSys/4.py
@@ -1,3 +1,24 @@
 """
 Напишите скрипт который принимает 2 аргумента - путь и имя папки. И создаем папку по указанному пути.
 """
+
+import sys
+import os
+
+if len(sys.argv) != 2:
+    sys.exit(1)
+
+fname = sys.argv[1]
+
+if not os.path.isfile(fname):
+    sys.exit(1)
+
+with open(fname) as f:
+    for line in f:
+        line = line.strip()
+
+        if not line or line.startswith("*"):
+            continue
+
+        print(line)
+        os.system(line)
